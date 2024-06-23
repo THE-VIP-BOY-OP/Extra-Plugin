f8aef161f2f1f34251b7e646ce4ea4c74b57badc
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-23 08:01:32 +0530

    Update welcomedb.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- utils/welcomedb.py
+++ utils/welcomedb.py
@@ -60,36 +60,7 @@ async def get_goodbye(chat_id: int) -> (str, str, str):
 # in used
 async def del_goodbye(chat_id: int):
     return await greetingsdb.delete_one({"chat_id": chat_id, "type": "goodbye"})
-
-# not in used
-async def is_greetings_clean_on(chat_id: int, greeting_type: str) -> bool:
-    data = await greetingsdb.find_one({"chat_id": chat_id, "type": greeting_type})
-    if not data:
-        return None
-
-    clean = data.get("clean", None)
-    if clean is None:
-        return None
-
-    return clean.lower() in ["yes", "on"]
-
-#  not in used
-async def set_greetings_clean(chat_id: int, greeting_type: str, clean: bool) -> bool:
-    clean_value = "on" if clean else "off"
-    result = await greetingsdb.update_one(
-        {"chat_id": chat_id, "type": greeting_type}, {"$set": {"clean": clean_value}}, upsert=True
-    )
-    return result.modified_count > 0 or result.upserted_id is not None
-
-# not in used
-
-async def get_old_greetings_message(chat_id: int, greeting_type: str) -> int:
-    return greeting_message[greeting_type].get(chat_id, None)
-
-# not in used
-async def set_old_greetings_message(chat_id: int, greeting_type: str, message_id: int):
-    greeting_message[greeting_type][chat_id] = message_id
-    return True
+ 
 
 # in used
 async def set_greetings_on(chat_id: int, type: str) -> bool:
@@ -117,14 +88,13 @@ async def is_greetings_on(chat_id: int, type: str) -> bool:
         type = "welcome_on"
     elif type == "goodbye":
         type = "goodbye_on"
-    
     data = await greetingsdb.find_one({"chat_id": chat_id})
     if not data:
         return False
-
-    greetings_on = data.get(type)
-    return greetings_on if greetings_on is not None else False
-
+    if data.get(type) is None:
+        return False
+    return True
+    
 # in used
 async def set_greetings_off(chat_id: int, type: str) -> bool:
     if type == "welcome":
