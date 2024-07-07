6e14e07a8063111bc0b330da3cc7ec18f308e23c
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-07 09:00:12 +0530

    Update info.py

--- plugins/info.py
+++ plugins/info.py
@@ -6,8 +6,36 @@ from pyrogram.types import Message
 from YukkiMusic import app
 from YukkiMusic.misc import SUDOERS
 from YukkiMusic.utils.database import is_gbanned_user
-from YukkiMusic.utils.sections import section
 
+n = "\n"
+w = " "
+
+bold = lambda x: f"**{x}:** "
+bold_ul = lambda x: f"**--{x}:**-- "
+
+mono = lambda x: f"`{x}`{n}"
+
+
+def section(
+    title: str,
+    body: dict,
+    indent: int = 2,
+    underline: bool = False,
+) -> str:
+    text = (bold_ul(title) + n) if underline else bold(title) + n
+
+    for key, value in body.items():
+        if value is not None:
+            text += (
+                indent * w
+                + bold(key)
+                + (
+                    (value[0] + n)
+                    if isinstance(value, list) and isinstance(value[0], str)
+                    else mono(value)
+                )
+            )
+    return text
 
 async def userstatus(user_id):
     try:
