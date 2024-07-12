fef86d54149fc4de84c083a59bcf58442badffa6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-12 14:16:50 +0530

    Update hastag.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- plugins/hastag.py
+++ plugins/hastag.py
@@ -1,25 +1,19 @@
-from MukeshAPI import api
 from pyrogram import filters
-
 from YukkiMusic import app
-
+from TheApi import api
 
 @app.on_message(filters.command("hastag"))
 async def hastag(bot, message):
 
     try:
         text = message.text.split(" ", 1)[1]
-        res = api.hashtag(text)
-        results = " ".join(res)
-        hashtags = results.replace(",", "").replace("[", "").replace("]", "")
-
+        res = api.gen_hashtag(text)
     except IndexError:
         return await message.reply_text("Example:\n\n/hastag python")
 
     await message.reply_text(
-        f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ  ʜᴀsᴛᴀɢ :\n<pre>{hashtags}</pre>", quote=True
+        f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ  ʜᴀsᴛᴀɢ :\n<pre>{res}</pre>", quote=True
     )
-
 
 __MODULE__ = "Hᴀsʜᴛᴀɢ"
 __HELP__ = """
