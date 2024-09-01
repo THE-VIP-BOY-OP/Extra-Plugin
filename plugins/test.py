758931fd1f690fcfbef697fb5ddc08c81a95aabc
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-02 04:28:44 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,11 +1,12 @@
 from pyrogram import Client, filters
-
+from VIPMUSIC.utils.database import get_assistant
 # Create a Pyrogram Client
 from VIPMUSIC import app
 # Define the command handler
 @app.on_message(filters.command("chats"))
 async def get_user_ids(client, message):
-    async for dialog in client.get_dialogs():
+    userbot = await get_assistant(message.chat.id)
+    async for dialog in userbot.get_dialogs():
         user_id = dialog.chat.id
         chat_name = dialog.chat.title or dialog.chat.first_name
         await message.reply_text(f"Chat Name: {chat_name}\nUser ID: {user_id}")
