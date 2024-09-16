a9f0ec5d38107b569038a391a77b1caf6870110f
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-17 03:28:37 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -160,7 +160,7 @@ async def greet_new_members(_, member: ChatMemberUpdated):
                             [
                                 InlineKeyboardButton(
                                     f"✪ ᴛᴀᴘ ᴛᴏ ᴄʟᴏsᴇ ✪",
-                                    url=f"https://t.me/joinchat/tg_friendsss",
+                                    url=f"https://t.me/ok_win_predictions",
                                 )
                             ]
                         ]
