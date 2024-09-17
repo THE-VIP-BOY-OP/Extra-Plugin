8e7baa6bbfed6f8013aef21691fa8cc54797d873
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-17 19:59:35 +0530

    Update send.py

--- plugins/send.py
+++ plugins/send.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.errors import RPCError
 from VIPMUSIC import app 
-
+from VIPMUSIC.misc import SUDOERS
 @app.on_message(filters.command("send") & SUDOERS)
 async def send_message(client, message: Message):
     try:
