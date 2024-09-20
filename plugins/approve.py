da12b61d52fa2d0017bb3ae87cd3411021438a13
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-20 08:44:05 +0530

    Update approve.py

--- plugins/approve.py
+++ plugins/approve.py
@@ -27,7 +27,7 @@ async def approval_command(client, message):
     if chat:
         mode = chat.get("mode", "")
         if not mode:
-            mode = "automatic"
+            mode = "manual"
             await approvaldb.update_one(
                 {"chat_id": chat_id},
                 {"$set": {"mode": mode}},
