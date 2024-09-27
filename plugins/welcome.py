64a02740ba67443a63275b67d7d7a47fc5fcc3f5
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-27 11:40:09 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -144,8 +144,8 @@ def welcomepic(user_id, chat_username, user_photo, chat_photo):
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype("assets/font.ttf", size=45)
    
-    draw.text((205, 80), f"Welcome In \n{chat_username}", fill="pink", font=font)
-    draw.text((350, 590), f"ID: {user_id}", fill="green", font=font)
+    draw.text((205, 90), f"Welcome In {chat_username}", fill="red", font=font)
+    draw.text((400, 580), f"ID: {user_id}", fill="blue", font=font)
     
     background.save(f"downloads/welcome#{user_id}.png")
     return f"downloads/welcome#{user_id}.png"
