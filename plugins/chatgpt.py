9053c393f9ac863ea124a283ab0c9839734f1ff6
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-30 15:28:29 +0530

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -28,7 +28,7 @@ async def chatgpt_chat_lang(bot, message):
     await message.reply_text(results)
 
 
-ww@app.on_message(filters.command(["chatgpt", "ai", "ask"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["chatgpt", "ai", "ask"]) & ~BANNED_USERS)
 async def chatgpt_chat(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
         await message.reply_text(
