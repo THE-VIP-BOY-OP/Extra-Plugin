240f1dea4ba8c3cde9282ae405cd37285a410569
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-30 15:26:02 +0530

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -6,7 +6,29 @@ from VIPMUSIC import app
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["chatgpt", "ai", "ask"]) & ~BANNED_USERS)
+@app.on_message(filters.command(["detect", "aidetect", "asklang"]) & ~BANNED_USERS)
+async def chatgpt_chat_lang(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text(
+            "Example:\n\n`/ai what is the lang of these sentences:- Hello, how are you?`"
+        )
+        return
+
+    if message.reply_to_message and message.reply_to_message.text:
+        user_text = message.reply_to_message.text
+    else:
+        user_text = " ".join(message.command[1:])
+
+    # Format the input to ask for language detection
+    user_input = f"what is the lang of these sentences: {user_text}"
+
+    # Send typing action and get the response from API
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+    results = api.chatgpt(user_input)
+    await message.reply_text(results)
+
+
+ww@app.on_message(filters.command(["chatgpt", "ai", "ask"]) & ~BANNED_USERS)
 async def chatgpt_chat(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
         await message.reply_text(
