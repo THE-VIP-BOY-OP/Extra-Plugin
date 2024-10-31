09cb1d85ec6677e858bacb03c33a363c73960eaa
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-10-31 16:37:56 +0530

    Update gemini.py

--- plugins/gemini.py
+++ plugins/gemini.py
@@ -42,19 +42,18 @@ async def chatbot_handler(client, message):
         return
 
     if (message.reply_to_message and message.reply_to_message.from_user.id == app.id) or not message.reply_to_message:
-            
-    try:
-        user_input = f"""
+        try:
+            user_input = f"""
                 text:- ({message.text})
                 text me message hai uske liye Ekdam chatty aur chhota reply do jitna chhota se chhota reply me kam ho jaye utna hi chota reply do agar jyada bada reply dena ho to maximum 1 line ka dena barna kosis krna chhota sa chhota reply ho aur purane jaise reply mat dena new reply lagna chahiye aur reply mazedar aur simple ho. Jis language mein yeh text hai, usi language mein reply karo. Agar sirf emoji hai toh bas usi se related emoji bhejo. Dhyaan rahe tum ek ladki ho toh reply bhi ladki ke jaise masti bhara ho.
                 Bas reply hi likh ke do, kuch extra nahi aur jitna fast ho sake utna fast reply do!
                 """
-        response = api.gemini(user_input)
-        await app.send_chat_action(message.chat.id, ChatAction.TYPING)
-        x = response["results"]
-        if x:
-            await message.reply_text(x, quote=True)
-        else:
-            await message.reply_text("sᴏʀʀʏ sɪʀ! ᴘʟᴇᴀsᴇ Tʀʏ ᴀɢᴀɪɴ")
-    except requests.exceptions.RequestException as e:
-        pass
+            response = api.gemini(user_input)
+            await app.send_chat_action(message.chat.id, ChatAction.TYPING)
+            x = response["results"]
+            if x:
+                await message.reply_text(x, quote=True)
+            else:
+                await message.reply_text("sᴏʀʀʏ sɪʀ! ᴘʟᴇᴀsᴇ Tʀʏ ᴀɢᴀɪɴ")
+        except requests.exceptions.RequestException as e:
+            pass
